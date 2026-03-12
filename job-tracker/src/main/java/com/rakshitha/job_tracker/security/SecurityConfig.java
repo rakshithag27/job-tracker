efc764a4c331d80ba806c5b965223e31c827d802
rakshithag27 <rakshithagreddy27@gmail.com>
2026-03-12 16:46:37 +0530

    Swagger UI Documentation added

--- job-tracker/src/main/java/com/rakshitha/job_tracker/security/SecurityConfig.java
+++ job-tracker/src/main/java/com/rakshitha/job_tracker/security/SecurityConfig.java
@@ -25,7 +25,7 @@ public class SecurityConfig {
                 .sessionManagement(session ->
                         session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/auth/**").permitAll()
+                        .requestMatchers("/api/auth/**", "/swagger-ui/**", "/v3/api-docs/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
